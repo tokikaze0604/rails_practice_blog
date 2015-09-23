28d705bf44bad3955e485a07f7ac04efaf80b7e2
tokikaze0604 <tokikaze0604@gmail.com>
2015-09-23 20:27:30 +0000

    5.11 finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
 
     def new
+        @article = Article.new
     end
 
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     def show
@@ -16,6 +20,20 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.all
+    end
+
+    def edit
+        @article = Article.find(params[:id]);
+    end
+
+    def update
+        @article = Article.find(params[:id]);
+
+        if @article.update(article_params)
+           redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'articles/edit'
+
   get 'articles/new'
 
   get 'welcome/index'
